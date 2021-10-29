e835d12152e6b89233e9ef90c7f4a05070ff2421
Varis Darasirikul <manjarb4@gmail.com>
2021-10-29 18:17:10 +0800

    Init Project

---
+++ UIKIT-fundamentals/StoryPromp/StoryPromp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoryPromp
+//
+//  Created by Varis Darasirikul on 29/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
