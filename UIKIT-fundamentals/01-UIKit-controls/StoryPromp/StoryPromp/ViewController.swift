e9497c01daab71bb73aa9b670e843f66fdccae97
Varis Darasirikul <manjarb4@gmail.com>
2021-11-03 18:06:00 +0800

    Add lesson 2

--- UIKIT-fundamentals/01-UIKit-controls/StoryPromp/StoryPromp/ViewController.swift
+++ UIKIT-fundamentals/01-UIKit-controls/StoryPromp/StoryPromp/ViewController.swift
@@ -8,7 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var nounTextField: UITextField!
+    @IBOutlet weak var adjectiveTextfield: UITextField!
+    @IBOutlet weak var verbTextField: UITextField!
+    @IBOutlet weak var numberSlider: UISlider!
+    
+    @IBAction func changeNumber(_ sender: UISlider) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
